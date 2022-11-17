07fab5d7c6768ec4fa4acb58da4ac9fa36895531
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2022-11-17 23:02:10 +0700

    Initial commit

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import Link from 'next/link';
+import style from './Navbar.module.css';
+
+function Navbar() {
+     const [clicked, setClicked] = useState(false);
+
+     return (
+          <>
+               <nav className={style.nav}>
+                    <Link id={style.navlogo} href="/">Starite</Link>
+
+                    <div>
+                         <ul
+                              id={style.navbar}
+                              className={clicked ? style.mobileActive : style.mobile}
+                         >
+                              <li>
+                                   <Link href="/">
+                                        Home
+                                   </Link>
+                              </li>
+                              <li>
+                                   <Link href="/creator">
+                                        Creahrefr
+                                   </Link>
+                              </li>
+                              <li>
+                                   <Link href="/projects">
+                                        Projects
+                                   </Link>
+                              </li>
+                         </ul>
+                    </div>
+
+                    <div id={style.mobile} onClick={() => {
+                         setClicked(!clicked)
+                    }}>
+                         <i
+                              id={style.bar}
+                              className={clicked ? "fas fa-times" : "fas fa-bars"}
+                         ></i>
+                    </div>
+               </nav>
+          </>
+     );
+}
+
+export default Navbar;
