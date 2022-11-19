6743abe1e0ee45c42863062a639181106d13ba39
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2022-11-19 12:45:05 +0800

    Update _error.js

--- pages/_error.js
+++ pages/_error.js
@@ -2,7 +2,7 @@ import style from '../styles/Error.module.css'
 
 function Error({ statusCode }) {
   return (
-    <div className={style.error-countainer}>
+    <div className={style.errorCountainer}>
       {statusCode
         ? <p>An error {statusCode} occurred on the server</p>
         : <p>An error occurred on client</p>}
