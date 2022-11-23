e8b35f5f9a4635889a420e7020cd8ea6fdc034b6
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2022-11-23 12:32:57 +0800

    Update FetchServerAPI.js

--- src/FetchData/FetchServerAPI.js
+++ src/FetchData/FetchServerAPI.js
@@ -17,10 +17,12 @@ export default function ServerReqAPI() {
                cancelToken: new axios.CancelToken(c => cancel = c)
           }).then(res => {
                setPlayer(res.data.players.online)
-               setLoading(true)
-          }).catch(e => {
-               if (axios.isCancel(e)) return
+               setLoading(false)
+          }).catch(err => {
+               if (axios.isCancel(err)) return
+               console.log(err)
                setError(true)
+               setLoading(false)
           })
 
           return () => cancel()
