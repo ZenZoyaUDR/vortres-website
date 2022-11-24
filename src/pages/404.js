787edc0831f832b0561360286b50723846484ddd
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2022-11-24 15:27:49 +0800

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,8 @@ import style from '../styles/Pages/Error.module.css'
 function NotFound() {
      return (
           <div className={style.error_container}>
-               <p className={style.error_message}>Sorry, the page you looking for does not exist.</p>
+               <p className={style.error_message}>Sorry,</p>
+               <p className={style.error_message}>the page you looking for does not exist.</p>
           </div>
      )
 }
