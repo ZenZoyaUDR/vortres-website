145975f896fb535eecad15765743f9fdde8e5c72
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2022-12-13 09:44:36 +0800

    Update and rename FetchServerAPI.js to getServerData.js

--- src/FetchData/getServerData.js
+++ src/FetchData/getServerData.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-export default function ServerReqAPI() {
+export default function getAPIData() {
      const [loading, setLoading] = useState(true);
      const [error, setError] = useState(false);
      const [player, setPlayer] = useState("-");
