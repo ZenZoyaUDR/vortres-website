d1121207141e470cf05fd14fd2bbfdd1a1d594b9
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2022-12-13 08:29:27 +0800

    Update FetchServerAPI.js

--- src/FetchData/FetchServerAPI.js
+++ src/FetchData/FetchServerAPI.js
@@ -1,33 +1,9 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import useSWR from 'swr';
+const url = 'https://jsonplaceholder.typicode.com/posts';
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export default function ServerReqAPI() {
-     const [loading, setLoading] = useState(true);
-     const [error, setError] = useState(false);
-     const [player, setPlayer] = useState("-");
+export const useGetData = () => {
+  const { data, error } = useSWR(url, fetcher);
 
-     useEffect(() => {
-          setLoading(true)
-          setError(false)
-
-          let cancel
-          axios({
-               method: 'GET',
-               url: 'https://api.mcsrvstat.us/2/mc.hypixel.net',
-               cancelToken: new axios.CancelToken(c => cancel = c)
-          }).then(res => {
-               setPlayer(res.data.players.online)
-               setLoading(false)
-          }).catch(err => {
-               if (axios.isCancel(err)) return
-               setError(true)
-               setLoading(false)
-          })
-
-          return () => cancel()
-     }, [])
-
-     return { loading, error, player }
-}
-
-setInterval(ServerReqAPI, 1000);
+  return { data, error };
+};
