35a52812878bf998595898dbdcdb6a5b5fab48b6
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-02-03 12:39:37 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
 
   return (
     <>
-      <div><Loading /></div>
+      {isLoading ? <div><Loading /></div>}
       <Head>
         <title>Starite | Home</title>
         <link rel="icon" href="/favicon.ico" />
