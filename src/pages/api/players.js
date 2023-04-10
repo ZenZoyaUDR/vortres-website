3e6c2bea2f53a12945f02f8c96ed0a0a70d00670
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-10 08:32:13 +0800

    Update players.js

--- src/pages/api/players.js
+++ src/pages/api/players.js
@@ -1,13 +1,35 @@
 import prisma from '../../lib/prisma'
 
 export default async function handler(req, res) {
-     const { search } = req.query;
-     const players = await prisma.player.findFirst({
-          where: {
-               username: {
-                    contains: search || "",
-               },
-          },
-     });
-     res.json(players);
+     const { search, findone } = req.query;
+
+     if(!search) return res.send("Search query required!")
+     if(findone === true ) {
+       const players = await prisma.player.findFirst({
+            where: {
+                 username: {
+                      contains: search || "",
+                 },
+            },
+       });
+       res.json(players);
+     } else if(findone === false) {
+       const players = await prisma.player.findMany({
+            where: {
+                 username: {
+                      contains: search || "",
+                 },
+            },
+       });
+       res.json(players);
+     } else {
+       const players = await prisma.player.findMany({
+            where: {
+                 username: {
+                      contains: search || "",
+                 },
+            },
+       });
+       res.json(players);
+     };
 }
