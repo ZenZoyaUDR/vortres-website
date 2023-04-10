28a109dfc8ae578226205124cfa9afef6d7c7f8b
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-10 10:10:20 +0800

    Update players.js

--- src/pages/api/players.js
+++ src/pages/api/players.js
@@ -7,23 +7,21 @@ export default async function handler(req, res) {
        res.status(400).json({ error: 'Missing query parameters' });
        return;
      }
-     if(findone) {
-       const players = await prisma.player.findFirst({
-            where: {
-                 username: {
-                      contains: search || "",
-                 },
-            },
-       });
-       res.json(players);
-     } else {
-       const players = await prisma.player.findMany({
-            where: {
-                 username: {
-                      contains: search || "",
-                 },
-            },
-       });
-       res.json(players);
+
+     try {
+       let data;
+
+       if (findone === 'true') {
+         data = await prisma.player.findFirst({ where: { username: search } });
+       } else {
+         data = await prisma.player.findMany({ where: { username: search } });
+       }
+
+       res.status(200).json({ data });
+     } catch (err) {
+       console.error(err);
+       res.status(500).json({ error: 'Internal server error' });
+     } finally {
+       await prisma.$disconnect();
      }
 }
