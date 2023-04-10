c196a9574b2dadd052facdf406a3fed14f236eb4
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-10 08:07:33 +0800

    Update player.js

--- src/pages/player.js
+++ src/pages/player.js
@@ -1,25 +1,17 @@
 import { useState } from 'react';
 import useSWR from 'swr';
 
-const fetcher = async (url) => {
-     const res = await fetch(url);
-     return res.json();
-};
-
 export default function Player() {
      const [query, setQuery] = useState("");
-     const [loading, setLoading] = useState(false);
 
-     const { data, error, isLoading } = useSWR(`/api/players?search=${query}`, async (url) => {
+     const { data, error, isLoading } = useSWR(`/api/players?search=${query}&findone=true`, async (url) => {
           const response = await fetch(url);
           return response.json();
      });
 
      const handleSearch = (event) => {
           event.preventDefault();
-          setLoading(true);
           setQuery(event.target.search.value);
-          setLoading(false);
      };
 
      return (
@@ -27,6 +19,7 @@ export default function Player() {
                <form onSubmit={handleSearch}>
                     <input type="text" name="search" />
                     <button type="submit">Search</button>
+                    {!query && <p>Please enter a search term</p>}
                </form>
 
                {isLoading ? (
