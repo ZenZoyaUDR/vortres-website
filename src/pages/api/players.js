944651f1bb793b05138b59a2f28f8a2b69eb8bd7
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-10 11:45:35 +0800

    Update players.js

--- src/pages/api/players.js
+++ src/pages/api/players.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
            }
          });
 
-       res.status(200).json({ data });
+       res.status(200).json(data);
      } catch (err) {
        console.error(err);
        res.status(500).json({ error: 'Internal server error' });
