e8ca530ef7ee1769c98b0a2167b1ec8eb36a3a04
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-11 10:25:01 +0800

    Update player.js

--- src/pages/player.js
+++ src/pages/player.js
@@ -1,55 +1,36 @@
 import { useState } from 'react';
 import useSWR from 'swr';
 
-import Tooltip from '../components/Tooltip/Tooltip';
-
 export default function Player() {
      const [query, setQuery] = useState("");
+     const [err, setErr] = useState(false);
 
      const { data, error, isLoading } = useSWR(`/api/players?search=${query}`, async (url) => {
           const response = await fetch(url);
           return response.json();
      });
 
-     const handleInputChange = (e) => {
-       const regex = /^[a-zA-Z0-9_]*$/;
-       const isValid = regex.test(e.target.value);
-       if (isValid) {
-         setQuery(e.terget.value)
-       }
-     };
+     const handleSearch = (event) => {
+          event.preventDefault();
 
-     const handleInputBlur = (e) => {
-       const regex = /^[a-zA-Z0-9_]*$/;
-       const isValid = regex.test(e.target.value);
-       if (!isValid) {
-         e.target.classList.add("invalid");
-       }
-     };
-
-     const handleInputFocus = (e) => {
-       e.target.classList.remove("invalid");
+          const regex = /^[A-Za-z0-9_]+$/;
+          const input = event.target.search.value
+          if (regex.test(input)) {
+            setQuery(event.target.search.value);
+            setErr(false);
+          } else {
+            setErr(true);
+          }
      };
 
      return (
           <div>
-               <div>
-                     <input
-                       type="text"
-                       value={query}
-                       onChange={handleInputChange}
-                       onBlur={handleInputBlur}
-                       onFocus={handleInputFocus}
-                     />
-                    {!searchTerm && (
-                      <Tooltip text="Please enter a search term">
-                        <button disabled>Search</button>
-                      </Tooltip>
-                    )}
-                    {searchTerm && (
-                      <button onClick={() => console.log("search clicked")}>Search</button>
-                    )}
-               </div>
+               <form onSubmit={handleSearch}>
+                    <input type="text" name="search" />
+                    <button type="submit">Search</button>
+                    {!query && <p>Please enter a search term</p>}
+                    {err && <p>Accepted characters: A-Z, a-z, 1-9, and _</p>}
+               </form>
 
                {isLoading ? (
                     <div>Loading...</div>
