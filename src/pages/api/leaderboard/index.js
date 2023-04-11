98106542684d992df1ca27ae5a368171755431d5
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-11 11:39:27 +0800

    Create index.js

---
+++ src/pages/api/leaderboard/index.js
@@ -0,0 +1,15 @@
+import prisma from '../../../lib/prisma';
+
+export default async function handler(req, res) {
+  try {
+    const players = await prisma.player.findMany({
+      select: {
+        username: true,
+        level: true,
+      },
+      orderBy: {
+        level: 'desc',
+      },
+      take: 10,
+    });
+}
