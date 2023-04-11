4411d7ed5562396d5b563476aaedc6e76edd6ddd
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-11 12:14:22 +0800

    Update index.js

--- src/pages/api/leaderboard/index.js
+++ src/pages/api/leaderboard/index.js
@@ -6,6 +6,7 @@ export default async function handler(req, res) {
       select: {
         username: true,
         level: true,
+        exp: true,
       },
       orderBy: {
         level: 'desc',
