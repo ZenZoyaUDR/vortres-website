dd81b0a31d7ad54762c1127f8e5bcf973d9a481b
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-12 10:21:58 +0800

    Update index.js

--- src/pages/api/leaderboard/index.js
+++ src/pages/api/leaderboard/index.js
@@ -17,5 +17,7 @@ export default async function handler(req, res) {
     res.status(200).json(players);
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    await prisma.$disconnect();
   }
 }
