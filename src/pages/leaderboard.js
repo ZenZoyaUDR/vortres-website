fad1490d52d796ed596f5ed3d44ae85edd2557c3
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-13 07:10:03 +0700

    ui update

--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -1,5 +1,10 @@
-import { useState, useEffect } from "react";
 import style from '../styles/Pages/Leaderboard.module.css';
+import { useState, useEffect } from "react";
+import Head from "next/head";
+
+// Components
+import Navbar from '../components/Navbar/Navbar';
+import Loading from '../components/Loading/Loading';
 
 export default function Leaderboard() {
   const [players, setPlayers] = useState([]);
@@ -12,28 +17,40 @@ export default function Leaderboard() {
   }, []);
 
   return (
-    <div className={style.lb_Container}>
-      <h1 className={style.lb_Heading}>Leaderboard</h1>
-      <table className={style.lb_Table}>
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Username</th>
-            <th>Lvl</th>
-            <th>Exp</th>
-          </tr>
-        </thead>
-        <tbody>
-          {players.map((player, index) => (
-            <tr key={player.id}>
-              <td>{index + 1}</td>
-              <td>{player.username}</td>
-              <td>{player.level}</td>
-              <td>{player.exp}</td>
+    <>
+      {players && <Loading />}
+      <Head>
+        <title>Vortres | Leaderboard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <div className={style.lb_Container}>
+        <h1 className={style.lb_Heading}>Leaderboard</h1>
+        <table className={style.lb_Table}>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Username</th>
+              <th>Lvl</th>
+              <th>Exp</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          {players && (
+            <tbody>
+              {players.map((player, index) => (
+                <tr key={player.id}>
+                  <td>{index + 1}</td>
+                  <td data-label="Username">{player.username}</td>
+                  <td data-label="Lavel">{player.level}</td>
+                  <td data-label="Experience">{player.exp}</td>
+                </tr>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
+    </>
   );
 }
