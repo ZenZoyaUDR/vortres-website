19bac6a9a5d94dc310696a95b88e1cb0deca6d9a
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-13 11:26:06 +0800

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,16 +5,6 @@ function Footer() {
   return (
     <div className={style.footer_container}>
     <footer className={style.footer}>
-      <a
-        href="https://vercel.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <span className={style.logo}>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={62} height={7} />
-        </span>
-      </a>
       <p className={style.copyright}>© 2023 Vortres</p>
     </footer>
     </div>
