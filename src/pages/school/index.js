bccb8225ae82b8be6eb300fe7a2033786530687a
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-14 13:41:40 +0800

    Update index.js

--- src/pages/school/index.js
+++ src/pages/school/index.js
@@ -1,1 +1,53 @@
+export default function Player() {
+     const [res, setRes] = use
 
+     const handleSearch = (event) => {
+          event.preventDefault();
+
+          const result = event.target.panjang.value*event.target.lebar.value;
+          setRes(result);
+     }
+
+     return (
+          <>
+               <Head>
+                    <title>Vortres | Stats</title>
+                    <link rel="icon" href="/favicon.ico" />
+               </Head>
+
+               <Navbar />
+
+               <div>
+                    <form onSubmit={handleSearch}>
+                         <div>
+                              <input
+                                   type="number"
+                                   placeholder="Search..."
+                                   name="panjang"
+                              />
+                              <input
+                                   type="number"
+                                   placeholder="Search..."
+                                   name="lebar"
+                              />
+                              <button type="submit">
+                                   Cal
+                              </button>
+                              {!res && <p>Please enter a search term</p>}
+                         </div>
+                    </form>
+
+                    {res ? (
+                         <ul>
+                              <li key={res}>
+                                   Selesai - Level: {res}
+                              </li>
+                         </ul>
+                    ) : (
+                         <div>Error</div>
+                    )}
+               </div>
+               <Footer />
+          </>
+     );
+}
