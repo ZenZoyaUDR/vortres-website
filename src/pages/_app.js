9f5d1c42038c18e9b138af56720bff00c84ceb6d
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-15 07:40:12 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/Global.css'
+import '../styles/Global.css';
+import dynamic from 'next/dynamic';
+
+const Footer = dynamic(() => import('../components/Footer/Footer'));
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+           <>
+             <Component {...pageProps} />
+             <Footer />
+           </>
+         )
 }
 
 export default App
