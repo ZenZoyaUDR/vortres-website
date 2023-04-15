8cad60094ea6002dc7e1ff0fcc3f9ce21cd33010
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-04-15 11:53:27 +0800

    Update index.js

--- src/pages/api/leaderboard/index.js
+++ src/pages/api/leaderboard/index.js
@@ -1,26 +1,26 @@
-import prisma from '../../../lib/prisma';
 import limiter from '../../../lib/ratelimit';
+import prisma from '../../../lib/prisma';
 
 export default async function handler(req, res) {
-  limiter(req, res, () => {
-    try {
-      const players = prisma.player.findMany({
-        select: {
-          username: true,
-          level: true,
-          exp: true,
-        },
-        orderBy: {
-          level: 'desc',
-        },
-        take: 10,
-      });
+  try {
+    await limiter(req, res);
 
-      res.status(200).json(players);
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
-    } finally {
-      prisma.$disconnect();
-    }
-  });
+    const players = await prisma.player.findMany({
+      select: {
+        username: true,
+        level: true,
+        exp: true,
+      },
+      orderBy: {
+        level: 'desc',
+      },
+      take: 10,
+    });
+
+    res.status(200).json(players);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    await prisma.$disconnect();
+  }
 }
